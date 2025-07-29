08e30d752b4f1a016910ac0d2abfc504773c5a06
Felix Bedard <felixbedard@Felixs-MacBook-Air.local>
2025-07-29 11:27:22 -0400

    Mon site Finexcorp avec Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screen: {
+      sm:"480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px"
+    }, extend: {
+
+      colors : {
+        brightRed: "hsl(12,88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        verydarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        teaGreen: "#C4F1BE",
+        cambridgeGreen: "#A2C3A4",
+        cambridgeBlue: "#869D96",
+        paynesGray: "#525B76",
+        paynesPale: "#6A7390",
+        paynesTrespale: "#d6d9e4ff",
+        finBleufonce: "#0F3A6A",
+        finBleupale: "#29548A",
+        finGold: "#D4AF37",
+        
+        
+
+      }
+    },
+   },
+  plugins: [],
+}
